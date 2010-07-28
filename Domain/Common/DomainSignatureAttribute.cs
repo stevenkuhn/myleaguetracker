d113e853c6c87595f1dc9987ffe7a7b14c48765b
Steven Kuhn <email@stevenkuhn.net>
2010-07-28 08:37:29 -0500

    Added implementation for ObjectBase, Entity, and ValueObject.

---
+++ Domain/Common/DomainSignatureAttribute.cs
@@ -0,0 +1,29 @@
+﻿#region License
+// 
+// Originally from S#arp Architechture (http://www.sharparchitecture.net/).
+// Source code at http://github.com/codai/Sharp-Architecture.
+// Licensed under the New BSD License from Codai, Inc.
+// Copyright (c) 2009, Codai, Inc.
+//
+// Modified by Steven Kuhn <email@stevenkuhn.net>.
+// Copyright (c) 2010, Steven Kuhn.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLeagueTracker.Domain.Common
+{
+	/// <summary>
+	/// Facilitates indicating which property(s) describe the unique signature of an 
+	/// entity.  See Entity.GetTypeSpecificSignatureProperties() for when this is leveraged.
+	/// </summary>
+	/// <remarks>
+	/// This is intended for use with <see cref="Entity" />.  It may NOT be used on a <see cref="ValueObject"/>.
+	/// </remarks>
+	[Serializable]
+	public class DomainSignatureAttribute : Attribute { }
+}
--- Domain/Common/ValueObject.cs
+++ Domain/Common/ValueObject.cs
@@ -1,10 +1,12 @@
 ﻿#region License
 // 
-// Author: Steven Kuhn <email@stevenkuhn.net>
+// Originally from S#arp Architechture (http://www.sharparchitecture.net/).
+// Source code at http://github.com/codai/Sharp-Architecture.
+// Licensed under the New BSD License from Codai, Inc.
+// Copyright (c) 2009, Codai, Inc.
+//
+// Modified by Steven Kuhn <email@stevenkuhn.net>.
 // Copyright (c) 2010, Steven Kuhn.
-// 
-// Licensed under the Apache License, Version 2.0.
-// See the file LICENSE for details.
 //
 #endregion
 
@@ -12,10 +14,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace MyLeagueTracker.Domain.Common
 {
+	/// <summary>
+	/// Provides a standard base class for facilitating comparison of value objects using all the object's properties.
+	/// 
+	/// For a discussion of the implementation of Equals/GetHashCode, see 
+	/// http://devlicio.us/blogs/billy_mccafferty/archive/2007/04/25/using-equals-gethashcode-effectively.aspx
+	/// and http://groups.google.com/group/sharp-architecture/browse_thread/thread/f76d1678e68e3ece?hl=en for 
+	/// an in depth and conclusive resolution.
+	/// </summary>
+	[Serializable]
 	public abstract class ValueObject : ObjectBase, IValueObject
 	{
+		/// <summary>
+		/// The getter for SignatureProperties for value objects should include the properties 
+		/// which make up the entirety of the object's properties; that's part of the definition 
+		/// of a value object.
+		/// </summary>
+		/// <remarks>
+		/// This ensures that the value object has no properties decorated with the 
+		/// [DomainSignature] attribute.
+		/// </remarks>
+		protected override IEnumerable<PropertyInfo> GetTypeSpecificSignatureProperties()
+		{
+			IEnumerable<PropertyInfo> invalidlyDecoratedProperties = GetType().GetProperties()
+				 .Where(p => Attribute.IsDefined(p, typeof(DomainSignatureAttribute), true));
+
+			if (invalidlyDecoratedProperties.Any())
+			{
+				throw new InvalidOperationException(
+					"Properties were found within " + GetType() + @" having the
+                [DomainSignature] attribute. The domain signature of a value object includes all
+                of the properties of the object by convention; consequently, adding [DomainSignature]
+                to the properties of a value object's properties is misleading and should be removed. 
+                Alternatively, you can inherit from Entity if that fits your needs better.");
+			}
+
+			return GetType().GetProperties();
+		}
 	}
 }
